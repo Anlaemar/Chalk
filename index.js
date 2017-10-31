01b28606fc96f9949ef53f377bac87afdfc28055
Adam <adam.d.goodwin@gmail.com>
2017-10-31 14:33:29 -0500

    Added short story with chalk colors

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const chalk = require('chalk');
 const chalkAnimation = require('chalk-animation');
+const gradient = require('gradient-string');
 
 // Spacer //
 console.log('');
 console.log('');
 
-console.log('For ' + chalk.bold('this ') + chalk.bgYellowBright.redBright('chalk ') + chalk.bgGreenBright.yellowBright('project') + ', I will be using a ' + chalk.magentaBright('prologue ') + 'from a story I began writting ' + chalk.underline.yellowBright('7 years ago. ') + chalkAnimation.rainbow('This was an attempt at writting a book that was never completed.') + chalk.italic.dim(' This story begins thousands of years ago...'));
+
+//console.log(gradient('cyan', 'pink')('Hello world!'));
+//chalkAnimation.rainbow('Lorem ipsum dolor sit amet');
+
+
+console.log('For ' + chalk.bold('this ') + chalk.bgYellowBright.redBright('chalk ') + chalk.bgGreenBright.yellowBright('project') + ', I will be using a ' + chalk.magentaBright('prologue ') + 'from a story I began writting ' + chalk.underline.yellowBright('7 years ago. ') + chalk.inverse('This was an attempt at writting a book that was never completed.') + chalk.italic.dim(' This story begins thousands of years ago...'));
 
 // Spacer //
 console.log('');
@@ -15,12 +21,12 @@ console.log('The ancient bloodline of ' + chalk.bgRedBright.yellowBright('Custos
 // Spacer //
 console.log('');
 
-console.log(chalk.inverse('Thousands of years later') + ', this power has awoken in a young man blessed with the drive to  ' + chalk.yellowBright('protect ') + ' every human and creature within  ' + chalk.bgCyanBright('Aeon') + ', just to have his father and his ruler taken away from him by the person closest to him. Having been blamed for the murder of the former ' + chalk.bgRedBright.yellowBright('Lord Custos') + ', the new leader of ' + chalk.bgCyanBright('Aeon') + ' is hell bent on the young man’s execution. The man’s new drive for justice and revenge reduces his inner power so he’s left powerless and leaves him no choice but to run.');
+console.log(chalk.inverse('Thousands of years later') + ', this power has awoken in a young man ' + chalk.bgWhiteBright.greenBright('blessed') + ' with the drive to ' + chalk.yellowBright('protect ') + ' every human and creature within  ' + chalk.bgCyanBright('Aeon') + ', just to have his father and his ruler ' + chalk.underline.bgMagentaBright.whiteBright('taken') + ' away from him by the person ' + chalk.redBright('closest ') + 'to him. Having been blamed for the ' + chalk.inverse.red('murder') + ' of the former ' + chalk.bgRedBright.yellowBright('Lord Custos') + ', the new leader of ' + chalk.bgCyanBright('Aeon') + ' is hell bent on the young man’s ' + chalk.inverse.red('execution') + '. The man’s new drive for justice and ' + chalk.inverse.red('revenge') + ' reduces his inner power so he’s left powerless and leaves him no choice but to ' + chalk.bgYellowBright.gray('run') + '.');
 
 // Spacer //
 console.log('');
 
-console.log('When he meets and falls in ' + chalk.red('love ') + 'with a ' + chalk.rgb(247,193,155)('women') +', his drive to protect her rekindles the power within and pushes it beyond the limits of the men of old, making him more powerful than ever imagined. Even that happiness is being threatened by his past. As emotions conflict, he makes a decision that will change the fate of many innocents, and the future of ' + chalk.bgCyanBright('Aeon') + '.');
+console.log('When he meets and falls in ' + chalk.red('love ') + 'with a ' + chalk.rgb(247,193,155)('women') +', his drive to protect her rekindles the power within and pushes it beyond the limits of the men of old, making him more powerful than ever imagined. Even that happiness is being threatened by his past. As ' + chalk.bgBlackBright.whiteBright('emotions') + ' ' + chalk.bgWhiteBright.gray('conflict') + ', he makes a ' + chalk.underline.bold('decision ') + 'that will change the fate of many innocents, and the future of ' + chalk.bgCyanBright('Aeon') + '.');
 
 // Spacer //
 console.log('');
