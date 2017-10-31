7762b07c7c448e391b7973d50afe4e6883ceb97e
Adam <adam.d.goodwin@gmail.com>
2017-10-31 14:58:11 -0500

    Changed a chalk per line

--- index.js
+++ index.js
@@ -11,40 +11,69 @@ console.log('');
 //chalkAnimation.rainbow('Lorem ipsum dolor sit amet');
 
 
-console.log('For ' + chalk.bold('this ') + chalk.bgYellowBright.redBright('chalk ') + chalk.bgGreenBright.yellowBright('project') + 
-            ', I will be using a ' + chalk.magentaBright('prologue ') + 'from a story I began writting ' + 
-            chalk.underline.yellowBright('7 years ago. ') + chalk.inverse('This was an attempt at writting a book that was never completed.') + 
-            chalk.italic.dim(' This story begins thousands of years ago...'));
+console.log('For ' + 
+            chalk.bold('this ') + 
+            chalk.bgYellowBright.redBright('chalk ') + 
+            chalk.bgGreenBright.yellowBright('project') + ', I will be using a ' + 
+            chalk.magentaBright('prologue ') + 'from a story I began writting ' + 
+            chalk.underline.yellowBright('7 years ago. ') + 
+            chalk.inverse('This was an attempt at writting a book that was never completed.'));
 
 // Spacer //
 console.log('');
 
-console.log('The ancient bloodline of ' + chalk.bgRedBright.yellowBright('Custos') + ' was ' + chalk.underline('blessed ') + 
-            'by the land of ' + chalk.bgCyanBright('Aeon') + ' with the power to ' + chalk.yellowBright('protect') + '; not to ' + 
-            chalk.inverse.red('conquer') + ', not to ' + chalk.inverse.red('intemidate') + ', but to ' + chalk.yellowBright('protect ') + 
-            'the ' + chalk.underline('land ') + 'and the ' + chalk.underline('people ') + 'within. As time progressed, the descendants ' + 
-            chalk.bgRedBright.yellowBright('Custos') + ' began to abuse their gift and the power ' + chalk.dim('quickly faded.'));
+console.log(chalk.italic.gray('This story begins thousands of years ago...'));
+
+// Spacer //
+console.log('');
+
+console.log(chalk.gray('The ancient bloodline ') + 'of ' + 
+            chalk.bgMagentaBright.whiteBright('Custos') + ' was ' + 
+            chalk.underline('blessed ') + 'by the land of ' + 
+            chalk.bgCyanBright('Aeon') + ' with the power to ' + 
+            chalk.yellowBright('protect') + '; not to ' + 
+            chalk.inverse.red('conquer') + ', not to ' + 
+            chalk.inverse.red('intemidate') + ', but to ' + 
+            chalk.yellowBright('protect ') + 'the ' + 
+            chalk.underline('land ') + 'and the ' + 
+            chalk.underline('people ') + 'within. As time progressed, the descendants ' + 
+            chalk.bgMagentaBright.whiteBright('Custos') + ' began to abuse their gift and the power ' + 
+            chalk.dim('quickly faded.'));
 
 // Spacer //
 console.log('');
 
 console.log(chalk.inverse('Thousands of years later') + ', this power has awoken in a young man ' + 
-            chalk.bgWhiteBright.greenBright('blessed') + ' with the drive to ' + chalk.yellowBright('protect ') + ' every human and creature within  ' + 
-            chalk.bgCyanBright('Aeon') + ', just to have his father and his ruler ' + chalk.underline.bgMagentaBright.whiteBright('taken') + 
-            ' away from him by the person ' + chalk.redBright('closest ') + 'to him. Having been blamed for the ' + chalk.inverse.red('murder') + 
-            ' of the former ' + chalk.bgRedBright.yellowBright('Lord Custos') + ', the new leader of ' + chalk.bgCyanBright('Aeon') + 
-            ' is hell bent on the young man’s ' + chalk.inverse.red('execution') + '. The man’s new drive for justice and ' + 
-            chalk.inverse.red('revenge') + ' reduces his inner power so he’s left powerless and leaves him no choice but to ' + 
+            chalk.bgWhiteBright.greenBright('blessed') + ' with the drive to ' + 
+            chalk.yellowBright('protect ') + 'every human and creature within  ' + 
+            chalk.bgCyanBright('Aeon') + ', just to have his father and his ruler ' + 
+            chalk.underline.bgMagentaBright.whiteBright('taken') + ' away from him by the person ' + 
+            chalk.redBright('closest ') + 'to him. Having been blamed for the ' + 
+            chalk.inverse.red('murder') + ' of the former ' + 
+            chalk.bgMagentaBright.whiteBright('Lord Custos') + ', the new leader of ' + 
+            chalk.bgCyanBright('Aeon') + ' is hell bent on the young man’s ' + 
+            chalk.inverse.red('execution') + '. The man’s new drive for justice and ' + 
+            chalk.inverse.red('revenge') + ' reduces his inner ' + 
+            chalk.bgYellowBright.redBright('power') + ' so he’s left ' + 
+            chalk.bgRedBright.yellowBright('powerless') + ' and leaves him no choice but to ' + 
             chalk.bgYellowBright.gray('run') + '.');
 
 // Spacer //
 console.log('');
 
-console.log('When he meets and falls in ' + chalk.red('love ') + 'with a ' + chalk.rgb(247,193,155)('women') +
-            ', his drive to protect her rekindles the power within and pushes it beyond the limits of the ' + chalk.bgWhiteBright.gray('men of old') + 
-            ', making him more powerful than ever imagined. Even that happiness is being threatened by his past. As ' + 
-            chalk.bgBlackBright.whiteBright('emotions') + ' ' + chalk.bgWhiteBright.gray('conflict') + ', he makes a ' + chalk.underline.bold('decision ') + 
-            'that will change the fate of many innocents, and the future of ' + chalk.bgCyanBright('Aeon') + '.');
+console.log('When he meets and falls in ' + 
+            chalk.red('love ') + 'with a ' + 
+            chalk.rgb(247,193,155)('women') + ', his drive to ' + 
+            chalk.yellowBright('protect ') + 'her rekindles the power within and pushes it beyond the limits of the ' + 
+            chalk.bgWhiteBright.gray('men of old') + ', making him more ' + 
+            chalk.bgYellowBright.redBright('powerful') + ' than ever imagined. Even that ' + 
+            chalk.gray('happiness ') + 'is being threatened by his ' + 
+            chalk.gray('past') + '. As ' + 
+            chalk.bgBlackBright.whiteBright('emotions') + ' ' + 
+            chalk.bgWhiteBright.gray('conflict') + ', he makes a ' + 
+            chalk.underline.bold('decision ') + 'that will ' + 
+            chalk.inverse('change the fate of many innocents') + ', and the future of ' + 
+            chalk.bgCyanBright('Aeon') + '.');
 
 // Spacer //
 console.log('');
