e8a9f9e46f887c5875c224935964791c79b7e7fe
Adam <adam.d.goodwin@gmail.com>
2017-10-31 14:11:51 -0500

    Addes short story and partial chalk code

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("hello world");
+const chalk = require('chalk');
+const chalkAnimation = require('chalk-animation');
+
+// Spacer //
+console.log('');
+console.log('');
+
+console.log('For ' + chalk.bold('this ') + chalk.bgYellowBright.redBright('chalk ') + chalk.bgGreenBright.yellowBright('project') + ', I will be using a ' + chalk.magentaBright('prologue ') + 'from a story I began writting ' + chalk.underline.yellowBright('7 years ago. ') + chalkAnimation.rainbow('This was an attempt at writting a book that was never completed.') + chalk.italic.dim(' This story begins thousands of years ago...'));
+
+// Spacer //
+console.log('');
+
+console.log('The ancient bloodline of ' + chalk.bgRedBright.yellowBright('Custos') + ' was ' + chalk.underline('blessed ') + 'by the land of ' + chalk.bgCyanBright('Aeon') + ' with the power to ' + chalk.yellowBright('protect') + '; not to ' + chalk.inverse.red('conquer') + ', not to ' + chalk.inverse.red('intemidate') + ', but to ' + chalk.yellowBright('protect ') + 'the ' + chalk.underline('land ') + 'and the ' + chalk.underline('people ') + 'within. As time progressed, the descendants ' + chalk.bgRedBright.yellowBright('Custos') + ' began to abuse their gift and the power ' + chalk.dim('quickly faded.'));
+
+// Spacer //
+console.log('');
+
+console.log(chalk.inverse('Thousands of years later') + ', this power has awoken in a young man blessed with the drive to  ' + chalk.yellowBright('protect ') + ' every human and creature within  ' + chalk.bgCyanBright('Aeon') + ', just to have his father and his ruler taken away from him by the person closest to him. Having been blamed for the murder of the former ' + chalk.bgRedBright.yellowBright('Lord Custos') + ', the new leader of ' + chalk.bgCyanBright('Aeon') + ' is hell bent on the young man’s execution. The man’s new drive for justice and revenge reduces his inner power so he’s left powerless and leaves him no choice but to run.');
+
+// Spacer //
+console.log('');
+
+console.log('When he meets and falls in ' + chalk.red('love ') + 'with a ' + chalk.rgb(247,193,155)('women') +', his drive to protect her rekindles the power within and pushes it beyond the limits of the men of old, making him more powerful than ever imagined. Even that happiness is being threatened by his past. As emotions conflict, he makes a decision that will change the fate of many innocents, and the future of ' + chalk.bgCyanBright('Aeon') + '.');
+
+// Spacer //
+console.log('');
+console.log('');
